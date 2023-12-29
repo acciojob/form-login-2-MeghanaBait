9758d5eedea1c53d82796e16af82653a8b8ba1e3
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-29 16:45:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function getFormValue(){
+	event.preventDefault();
+
+	let firstName = document.getElementsByName('fname')[0].value;
+    let lastName = document.getElementsByName('lname')[0].value;
+	let phone = document.getElementsByName('phone')[0].value;
+	let email = document.getElementsByName('email')[0].value;
+
+	alert("First Name: "firstName + '\nLastName :' + lastName + '\nPhone Number :' + phone + '\nEmail ID: ');
+}
