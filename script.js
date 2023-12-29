168391ea6751f566297f88e8a23e3d2d8f0698f0
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-29 16:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function getFormValue() {
+function getFormvalue() {
     event.preventDefault();
 
     let firstName = document.getElementsByName('First Name')[0].value;
     let lastName = document.getElementsByName('Last Name')[0].value;
-    let phone = document.getElementsByName('Phone Number')[0].value;
-    let email = document.getElementsByName('Email ID')[0].value;
+    let phoneNumber = document.getElementsByName('Phone Number')[0].value;
+    let emailID = document.getElementsByName('Email ID')[0].value;
 
-    alert("First Name: " + firstName + '\nLast Name: ' + lastName + '\nPhone Number: ' + phone + '\nEmail ID: ' + email);
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nPhone Number: " + phoneNumber + "\nEmail ID: " + emailID);
 }
